2f19998d70f1869056e454459db56a3200a09c1c
Tobías Nuñez <lpctobinz@gmail.com>
2024-06-30 20:29:51 -0300

    Agregar archivos de proyecto.

---
+++ ConsumoAPIs/ListaPaises.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace ConsumoAPIs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    internal class ListaPaises
+    {
+        [JsonProperty("success", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Success { get; set; }
+
+        [JsonProperty("error", NullValueHandling = NullValueHandling.Ignore)]
+
+        public string Error { get; set; }
+
+
+
+        [JsonProperty("result", NullValueHandling = NullValueHandling.Ignore)]
+        public List<Pais> listaPaises { get; set; }
+
+    }
+
+    public class Pais
+    {
+        [JsonProperty("country_key", NullValueHandling = NullValueHandling.Ignore)]
+        public string CountryKey { get; set; }
+
+        [JsonProperty("msg", NullValueHandling = NullValueHandling.Ignore)]
+
+        public string Message { get; set; }
+
+
+        [JsonProperty("country_name", NullValueHandling = NullValueHandling.Ignore)]
+        public string CountryName { get; set; }
+
+        [JsonProperty("country_iso2")]
+        public string CountryIso2 { get; set; }
+
+        [JsonProperty("country_logo", NullValueHandling = NullValueHandling.Ignore)]
+        public Uri CountryLogo { get; set; }
+    }
+}
